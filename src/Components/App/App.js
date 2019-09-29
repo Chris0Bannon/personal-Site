e1d34a274c60e2e796d8b29d1c8b55d45cf7c9ff
Christopher O'Bannon <Crobwan@gmail.com>
2019-09-29 14:40:01 -0500

    first commit

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {HashRouter as Router, Route} from 'react-router-dom';
+import Header from '../Header/Header';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
+        <Header />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hello and welcome to my personal Website!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
+      </Router>
     );
   }
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class Header extends Component {
+render(){
+return(
+    <h1> Hello from my Header</h1>
+)
+}
+}
+
+
+export default Header;
